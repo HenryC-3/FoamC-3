e103be9afd7cf449f372d03b1ecede88654c4d7e
HenryC-3 <2324375439@qq.com>
2021-03-31 14:09:35 +0800

    update: add vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,102 @@
+const path = require("path");
+const imsize = require("markdown-it-imsize");
+const wikilinks = require("vuepress-markdown-it-wikilink")({
+	baseURL: "/",
+	relativeBaseURL: "/",
+	htmlAttributes: { class: "wikilinks" },
+});
+
+module.exports = {
+	title: "Henry's Wiki",
+	description:
+		"Foam as second brain, for adversarial attacks and AI security. Powered by VuePress.",
+	head: [
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/favicon-32x32.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				href: "/favicon-16x16.png",
+			},
+		],
+		["link", { rel: "manifest", href: "/site.webmanifest" }],
+	],
+	theme: path.resolve(__dirname, "../theme"),
+	markdown: {
+		extendMarkdown: (md) => {
+			md.use(wikilinks).use(imsize);
+		},
+	},
+	plugins: [
+		["@vuepress/active-header-links"],
+		"@vuepress/plugin-nprogress",
+		"vuepress-plugin-mathjax",
+		[
+			"container",
+			{
+				type: "tip",
+				defaultTitle: "TIP",
+			},
+		],
+		[
+			"container",
+			{
+				type: "warning",
+				defaultTitle: "WARNING",
+			},
+		],
+		[
+			"container",
+			{
+				type: "danger",
+				defaultTitle: "DANGER",
+			},
+		],
+		[
+			"container",
+			{
+				type: "success",
+				defaultTitle: "SUCCESS",
+			},
+		],
+		[
+			"container",
+			{
+				type: "backlinks",
+				before: () => `<div class="custom-block backlinks">\n`,
+				after: () => "</div>\n",
+			},
+		],
+		[
+			"container",
+			{
+				type: "details",
+				before: (info) =>
+					`<details class="custom-block details">${
+						info ? `<summary>${info}</summary>` : ""
+					}\n`,
+				after: () => "</details>\n",
+			},
+		],
+		["smooth-scroll"],
+	],
+	patterns: ["**/*.md","!node_modules"]
+};
--- README.md
+++ README.md
@@ -1,27 +1,48 @@
 ### 前言
-[FoamC-3](https://github.com/HenryC-3/FoamC-3) 基于 [foambubble/foam-template: Foam workpace template](https://github.com/foambubble/foam-template)
+
+[FoamC-3](https://github.com/HenryC-3/FoamC-3) 基于:
+
+- [foambubble/foam-template: Foam workpace template](https://github.com/foambubble/foam-template)
+- [spencerwooo/foam-wiki: Foam as second brain. Powered by VuePress.](https://github.com/spencerwooo/foam-wiki)
 
 ### 项目目录
+
 ```text
+├── .vscode
+├── .vuepress
 ├── attachments
 ├── examples
-├── inbox.md 
+├── inbox.md
 ├── index.md
-└── references
-    └── course
+├── README.md
+├── theme
 ```
-- attachments: 储存图片
-- examples: 验证想法的例子
-- inbox.md: 想法收集箱
-- index.md: 根文件, 也是部署 `gitpage` 的首页
-- references: 文档库, 储存参考资料
-- course: 课程相关资料
+
+- .vscode: vscode 工作区配置文件
+- .vuepress: vuepress 配置文件
+- thmeme: vuepress 主题配置
+- attachments: 附件
+- examples: 示例
+- inbox.md: 收集箱
+- index.md: 根文件, 也是部署 `GitPage` 的首页
 
 ### 插件集合
+
 - foam: 提供 wikilink, note graph, backlink, tag 功能
-- vscode-pangu: 美化中文格式
 - markdown Preview mermaid: mermaid markdown 预览
 - Mermaid Markdown Syntax Highlighting: mermaid 语法高亮
 - vscode-drawio: 在 vscode 中使用 drawio, 相较于使用 drawio 桌面端, 使用该插件能够从 vscode 中无缝跳转至 drawio
 - vscode-drawio-mermaid-plugin: 支持在 drawio 中绘制 mermaid
 - TODO tree: 寻找文件的所有 TODO 并集中显示, 用于完成简单的 GTD 功能
+
+### 用法
+
+- 在项目根目录下新建 `docs` 文件夹, `.gitignore` 中添加 `./docs/node_modules`
+- 拉取仓库, 删除 `docs` 中 `.gitignore`
+- 使用 vscode 打开 `docs` 工作区, 安装推荐插件
+- 在 `index.md` 中组织笔记结构, 使用 foam `show graph` 或者 `orphan view` 查看零散笔记
+
+### Q&A
+
+1. 为什么删除 `docs` 文件夹下的 `gitignore` ?
+   - 于我而言, `docs` 中的内容是笔记, 相当于增强版 `git message` 因此与代码同步进行版本管理
